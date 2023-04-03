05237176e66242703e210ce33aa39c4384f26239
nath-d <nath.deborshi0711@gmail.com>
2023-04-03 17:40:47 +0530

    picture changed

--- src/Helpers/CarouselData.js
+++ src/Helpers/CarouselData.js
@@ -3,9 +3,9 @@ export const slides = [
     { text: "THIS IS SLIDE ONE",
       url:"https://i.ibb.co/StpmrFP/wherda-arsianto-l7d-P0-O8-Dj60-unsplash.jpg"},
     {text: "THIS IS SLIDE TWO",
-      url:"https://i.ibb.co/wR2W0Sy/waldemar-JYPDh4ter10-unsplash.jpg"},
+      url:"https://i.ibb.co/xS60fNc/wan-chen-c8-Lq-Vdqs0-HE-unsplash.jpg"},
     {text: "THIS IS SLIDE THREE",
-      url:"https://i.ibb.co/2YYQYvx/rishabh-sharma-R-js25-Pv1-LQ-unsplash.jpg"},
+      url:"https://i.ibb.co/QKY6d8b/hatice-yardim-gf-ZZVi-Txuqw-unsplash.jpg"},
     {text: "THIS IS SLIDE FOUR",
       url:"https://i.ibb.co/yYqxWNd/absolutvision-WYd-Pk-Ca1-BY-unsplash.jpg"}
     
--- src/screens/homepage/components/Carousel.jsx
+++ src/screens/homepage/components/Carousel.jsx
@@ -18,21 +18,21 @@ const Carousel = ({ autoSlide = false, autoSlideInterval = 6000 }) => {
     const slideInterval = setInterval(next, autoSlideInterval)
     return () => clearInterval(slideInterval)
   }, [])
-  
+
   //style={{backgroundImage: `url(${slides[curr].url})`}}
   //style={{transform: `translate(-${curr*100}%)`}}
 
   const style1 = { backgroundImage: `url(${slides[curr].url})` }
   // const style2={transform: `translate(-${slides[curr]*100}%)`}
-  
+
   // transition-transform ease-out duration-500
 
   return (
     <div className="carousel">
-      <div className="carouselInner" style={{ ...style1}}>
-          
-            <p className="text-8xl bg-black/50 carouselText font-semibold p-14">{slides[curr].text}</p>
-          
+      <div className="carouselInner" style={{ ...style1 }}>
+
+        <p className="text-8xl bg-black/50 carouselText font-semibold p-14">{slides[curr].text}</p>
+
         <div className="absolute inset-0 flex justify-between p-4">
           <button onClick={prev}>
             <FiChevronLeft size={40} />
