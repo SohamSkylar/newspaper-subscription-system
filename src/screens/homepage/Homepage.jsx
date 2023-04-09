4e5378bb93925c714f380126018b8eb57e42726c
Saptarshi1619 <d.saptarshi2001@gmail.com>
2023-04-09 22:26:52 +0530

    sections added

--- src/screens/homepage/Homepage.jsx
+++ src/screens/homepage/Homepage.jsx
@@ -4,6 +4,7 @@ import NewspaperCard from "./components/NewspaperCard.jsx";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import CarouselSlider from "./components/CarouselSlider.js";
+import "../homepage/components/OtherStyles.css";
 
 // https://i.ibb.co/StpmrFP/wherda-arsianto-l7d-P0-O8-Dj60-unsplash.jpg
 // https://i.ibb.co/wR2W0Sy/waldemar-JYPDh4ter10-unsplash.jpg
@@ -17,11 +18,16 @@ const Homepage = () => {
   return (
     <>
       <Navbar />
-      <div className="max-w-full">
+      <div className="max-w-full" id="homePage">
         <CarouselSlider/>
-        
+        <div className="btnclass">
+          <button className="btn"><a href="#homePage">Home</a></button>
+          <button className="btn"><a href="#cardSection">Cards</a></button>
+          <button className="btn"><a href="#">tbd</a></button>
+        </div>
       </div>
-      <div>
+
+      <div className="cardSection" id="cardSection">
         <NewspaperCard/>
       </div>
 
