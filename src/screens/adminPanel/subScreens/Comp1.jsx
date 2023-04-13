0b1b8c9dba28ddc18098a389e7bd18813530166a
nath-d <nath.deborshi0711@gmail.com>
2023-04-13 21:19:31 +0530

    company admin panel added

--- src/screens/adminPanel/subScreens/Comp1.jsx
+++ src/screens/adminPanel/subScreens/Comp1.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
 import Sidebar from "../components/Sidebar";
+import { Dialog, Transition } from "@headlessui/react";
+import { ImUserPlus } from "react-icons/im";
+import { useFormik } from "formik";
+import { Toaster, toast } from "react-hot-toast";
 
-const Comp1 = () => {
+const Comp1 = ({ open, setOpen }) => {
+  // const cancelButtonRef = useRef(null);
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      companyName: "",
+      companyId: "",
+      img1: "",
+      img2: ""
+    },
+    validate: false,
+    validateOnBlur: false,
+    validateOnChange: false,
+    // onSubmit: async (values) => {
+    //   let toastBox = toast.loading("Loading...");
+    //   let loginPromise = loginUser(values);
+    //   loginPromise.then(
+    //     (resolve) => {
+    //       toast.success("Logged in Successfully!", {
+    //         id: toastBox,
+    //       });
+    //       localStorage.setItem("token", resolve);
+    //         setOpen(false);
+    //     },
+    //     (msg) => {
+    //       toast.error(`${msg}`, {
+    //         id: toastBox,
+    //       });
+    //         setOpen(false);
+    //     }
+    //   );
+    // },
+  });
   return (
     <div className="flex">
       <Sidebar />
@@ -14,33 +51,75 @@ const Comp1 = () => {
             This information will be displayed publicly so be careful what you
             share.
           </p>
-
+        <form onSubmit={formik.handleSubmit}>
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
               <label
                 for="username"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Username
+                Company Name
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-                    workcation.com/
-                  </span>
+                  
                   <input
                     type="text"
+                    required
                     name="username"
                     id="username"
                     autocomplete="username"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="janesmith"
+                    placeholder="Enter Company Name"
+                    {...formik.getFieldProps("comanyName")}
                   />
                 </div>
               </div>
             </div>
+            <div className="sm:col-span-4">
+              <label
+                for="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Company ID
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  
+                  <input
+                    type="text"
+                    required
+                    name="username"
+                    id="username"
+                    autocomplete="username"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Enter Company ID"
+                    {...formik.getFieldProps("comanyId")}
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="sm:col-span-4">
+              <label
+                for="email"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div class="mt-2">
+                <input
+                  {...formik.getFieldProps("email")}
+                  id="email"
+                  required
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-            <div className="col-span-full">
+            {/* <div className="col-span-full">
               <label
                 for="about"
                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -58,9 +137,9 @@ const Comp1 = () => {
               <p className="mt-3 text-sm leading-6 text-gray-600">
                 Write a few sentences about yourself.
               </p>
-            </div>
+            </div> */}
 
-            <div className="col-span-full">
+            {/* <div className="col-span-full">
               <label
                 for="photo"
                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -87,14 +166,14 @@ const Comp1 = () => {
                   Change
                 </button>
               </div>
-            </div>
+            </div> */}
 
             <div className="col-span-full">
               <label
                 for="cover-photo"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Cover photo
+                Upload photo
               </label>
               <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                 <div className="text-center">
@@ -121,6 +200,8 @@ const Comp1 = () => {
                         name="file-upload"
                         type="file"
                         className="sr-only"
+                        {...formik.getFieldProps("img1")}
+                        required
                       />
                     </label>
                     <p className="pl-1">or drag and drop</p>
@@ -132,6 +213,53 @@ const Comp1 = () => {
               </div>
             </div>
           </div>
+          <div className="col-span-full">
+              <label
+                for="cover-photo"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Upload photo (Optional)
+              </label>
+              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                <div className="text-center">
+                  <svg
+                    className="mx-auto h-12 w-12 text-gray-300"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                    <label
+                      for="file-upload"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                    >
+                      <span>Upload a file</span>
+                      <input
+                        {...formik.getFieldProps("img2")}
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        className="sr-only"
+                      />
+                    </label>
+                    <p className="pl-1">or drag and drop</p>
+                  </div>
+                  <p className="text-xs leading-5 text-gray-600">
+                    PNG, JPG, GIF up to 10MB
+                  </p>
+                </div>
+              </div>
+              <div>
+                <button>SUBMIT</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
