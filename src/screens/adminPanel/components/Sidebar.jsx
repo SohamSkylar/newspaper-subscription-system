67f8ac40fc13006a070f784f930922289c06e3cd
SohamSkylar <sohamsarkar2001@gmail.com>
2023-04-15 23:10:19 +0530

    sign in/out added

--- src/screens/adminPanel/components/Sidebar.jsx
+++ src/screens/adminPanel/components/Sidebar.jsx
@@ -5,22 +5,68 @@ import { RiSettings4Line } from "react-icons/ri";
 import { TbReportAnalytics } from "react-icons/tb";
 import { AiOutlineUser, AiOutlineHeart } from "react-icons/ai";
 import { FiMessageSquare, FiFolder, FiShoppingCart } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { activeUser } from "../../../helpers/CustomerApi";
+import AdminSignIn from "../../../globalComponents/AdminSignIn";
 
 const Sidebar = () => {
-  const menus = [
-    { name: "dashboard", link: "/adminpanel/partner", icon: MdOutlineDashboard },
-    { name: "user", link: "/adminpanel/paper", icon: AiOutlineUser },
-    { name: "messages", link: "/adminpanel/subscription", icon: FiMessageSquare },
-    { name: "analytics", link: "/", icon: TbReportAnalytics, margin: true },
-    { name: "File Manager", link: "/", icon: FiFolder },
-    { name: "Cart", link: "/", icon: FiShoppingCart },
-    { name: "Saved", link: "/", icon: AiOutlineHeart, margin: true },
-    { name: "Setting", link: "/", icon: RiSettings4Line },
-  ];
+  const [menuItem, setMenuItem] = useState([]);
+  const [signInModal, setSignInModal] = useState(false);
+  const navigate = useNavigate();
+
   const [open, setOpen] = useState(true);
+  const [isAdmin, setIsAdminVal] = useState(false);
+
+  const handleSignInModal = () => {
+    setSignInModal(true);
+  };
+
+  const handledeleteToken = () => {
+    localStorage.removeItem("token");
+    setTimeout(() => {
+      navigate("/adminpanel");
+    }, 500) 
+  };
 
   useEffect(() => {
+    const adminMenu = [
+      {
+        name: "dashboard",
+        link: "/adminpanel/partner",
+        icon: MdOutlineDashboard,
+      },
+      { name: "user", link: "/adminpanel/paper", icon: AiOutlineUser },
+      {
+        name: "messages",
+        link: "/adminpanel/subscription",
+        icon: FiMessageSquare,
+      },
+      { name: "analytics", link: "/", icon: TbReportAnalytics, margin: true },
+      { name: "File Manager", link: "/", icon: FiFolder },
+      { name: "Cart", link: "/", icon: FiShoppingCart },
+      { name: "Saved", link: "/", icon: AiOutlineHeart, margin: true },
+      { name: "Setting", link: "/", icon: RiSettings4Line }
+    ];
+
+    const nonAdminMenu = [{ name: "Sign in", icon: RiSettings4Line }];
+
+    const activeUserFunc = () => {
+      const activeUserPromise = activeUser();
+      activeUserPromise
+        .then((data) => {
+          // console.log(data)
+          if (data.type === "admin") {
+            setIsAdminVal(true);
+            setMenuItem(adminMenu);
+          } else {
+            setMenuItem(nonAdminMenu);
+          }
+        })
+        .catch((err) => {
+          setMenuItem(nonAdminMenu);
+        });
+    };
+    activeUserFunc();
     function handleResize() {
       if (window.innerWidth > 900) {
         setOpen(true);
@@ -34,52 +80,114 @@ const Sidebar = () => {
   }, []);
 
   return (
-    <section className="flex gap-6">
-      <div
-        className={`bg-[#0e0e0e] min-h-screen ${
-          open ? "w-72" : "w-16"
-        } duration-500 text-gray-100 px-4`}
-      >
-        <div className="py-3 flex justify-end">
-          <HiMenuAlt3
-            size={26}
-            className="cursor-pointer"
-            onClick={() => setOpen(!open)}
-          />
-        </div>
-        <div className="mt-4 flex flex-col gap-4 relative">
-          {menus?.map((menu, i) => (
-            <Link
-              to={menu?.link}
-              key={i}
-              className={` ${
-                menu?.margin && "mt-5"
-              } group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
-            >
-              <div>{React.createElement(menu?.icon, { size: "20" })}</div>
-              <h2
-                style={{
-                  transitionDelay: `${i + 3}00ms`,
-                }}
-                className={`whitespace-pre duration-500 ${
-                  !open && "opacity-0 translate-x-28 overflow-hidden"
-                }`}
+    <>
+      <section className="flex gap-6">
+        <div
+          className={`bg-[#0e0e0e] min-h-screen ${
+            open ? "w-72" : "w-16"
+          } duration-500 text-gray-100 px-4`}
+        >
+          <div className="py-3 flex justify-end">
+            <HiMenuAlt3
+              size={26}
+              className="cursor-pointer"
+              onClick={() => setOpen(!open)}
+            />
+          </div>
+          <div className="mt-4 flex flex-col gap-4 relative">
+            {isAdmin
+              ? menuItem?.map((menu, i) => (
+                  <Link
+                    to={menu?.link}
+                    key={i}
+                    className={` ${
+                      menu?.margin && "mt-5"
+                    } group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
+                  >
+                    <div>{React.createElement(menu?.icon, { size: "20" })}</div>
+                    <h2
+                      style={{
+                        transitionDelay: `${i + 3}00ms`,
+                      }}
+                      className={`whitespace-pre duration-500 ${
+                        !open && "opacity-0 translate-x-28 overflow-hidden"
+                      }`}
+                    >
+                      {menu?.name}
+                    </h2>
+                    <h2
+                      className={`${
+                        open && "hidden"
+                      } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
+                    >
+                      {menu?.name}
+                    </h2>
+                  </Link>
+                ))
+              : menuItem?.map((menu, i) => (
+                  <Link
+                    onClick={handleSignInModal}
+                    key={i}
+                    className={` ${
+                      menu?.margin && "mt-5"
+                    } group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
+                  >
+                    <div>{React.createElement(menu?.icon, { size: "20" })}</div>
+                    <h2
+                      style={{
+                        transitionDelay: `${i + 3}00ms`,
+                      }}
+                      className={`whitespace-pre duration-500 ${
+                        !open && "opacity-0 translate-x-28 overflow-hidden"
+                      }`}
+                    >
+                      {menu?.name}
+                    </h2>
+                    <h2
+                      className={`${
+                        open && "hidden"
+                      } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
+                    >
+                      {menu?.name}
+                    </h2>
+                  </Link>
+                ))}
+            {isAdmin ? (
+              <Link
+                onClick={handledeleteToken}
+                className="group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md"
               >
-                {menu?.name}
-              </h2>
-              <h2
-                className={`${
-                  open && "hidden"
-                } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
-              >
-                {menu?.name}
-              </h2>
-            </Link>
-          ))}
+                <div>
+                  {React.createElement(RiSettings4Line, { size: "20" })}
+                </div>
+                <h2
+                  style={{
+                    transitionDelay: `${9 + 3}00ms`,
+                  }}
+                  className={`whitespace-pre duration-500 ${
+                    !open && "opacity-0 translate-x-28 overflow-hidden"
+                  }`}
+                >
+                  Sign out
+                </h2>
+                <h2
+                  className={`${
+                    open && "hidden"
+                  } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
+                >
+                  Sign out
+                </h2>
+              </Link>
+            ) : null}
+          </div>
         </div>
-      </div>
-      <div className="m-3 text-xl text-gray-900 font-semibold"></div>
-    </section>
+        <div className="m-3 text-xl text-gray-900 font-semibold"></div>
+      </section>
+      <AdminSignIn
+        open={signInModal}
+        setOpen={(bool) => setSignInModal(bool)}
+      />
+    </>
   );
 };
 
--- src/screens/adminPanel/subScreens/ManageSubscription.jsx
+++ src/screens/adminPanel/subScreens/ManageSubscription.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
 import { useFormik } from "formik";
-import { addPaperSub, addSubType, showAllSubType } from "../../../helpers/SubscriptionApi";
+import {
+  addPaperSub,
+  addSubType,
+  showAllSubType,
+} from "../../../helpers/SubscriptionApi";
 import { Toaster, toast } from "react-hot-toast";
 import { useState } from "react";
 import { useEffect } from "react";
 import { showAllPapers } from "../../../helpers/NewspaperApi";
+import { activeUser } from "../../../helpers/CustomerApi";
 
 const ManageSubscription = () => {
+  const [isAdmin, setIsAdminVal] = useState(false);
+
+  const activeUserFunc = () => {
+    const activeUserPromise = activeUser();
+    activeUserPromise
+      .then((data) => {
+        // console.log(data)
+        if (data.type === "admin") {
+          setIsAdminVal(true);
+        } else {
+          setIsAdminVal(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const subTypeFormik = useFormik({
     initialValues: {
       sub_name: "",
@@ -73,186 +96,196 @@ const ManageSubscription = () => {
     onSubmit: async (values) => {
       let toastBox = toast.loading("Loading...");
       let registerPromise = addPaperSub(values);
-      registerPromise.then(
-        () => {
-          toast.success("Added Successfully!", {
+      registerPromise
+        .then(
+          () => {
+            toast.success("Added Successfully!", {
+              id: toastBox,
+            });
+          },
+          (msg) => {
+            toast.error(`${msg}`, {
+              id: toastBox,
+            });
+          }
+        )
+        .catch((err) => {
+          toast.error(`${err}`, {
             id: toastBox,
           });
-        },
-        (msg) => {
-          toast.error(`${msg}`, {
-            id: toastBox,
-          });
-        }
-      ).catch((err) => {
-        toast.error(`${err}`, {
-          id: toastBox,
         });
-      })
     },
   });
 
   useEffect(() => {
     getSubIDFunc();
     getPaperIDFunc();
+    activeUserFunc();
   }, []);
 
   return (
     <div className="flex">
       <Toaster position="top-center" reverseOrder={false}></Toaster>
       <Sidebar />
-      <div>
-        <div class="border-b border-gray-900/10 pb-12 p-14">
-          <h2 class="text-base font-semibold leading-7 text-gray-900">
-            Subscription Type
-          </h2>
-          <p class="mt-1 text-sm leading-6 text-gray-600">
-            We'll always let you know about important changes, but you pick what
-            else you want to hear about.
-          </p>
-          <form onSubmit={subTypeFormik.handleSubmit}>
-            <div class="mt-10 space-y-10">
-              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+      {isAdmin ? (
+        <div>
+          <div class="border-b border-gray-900/10 pb-12 p-14">
+            <h2 class="text-base font-semibold leading-7 text-gray-900">
+              Subscription Type
+            </h2>
+            <p class="mt-1 text-sm leading-6 text-gray-600">
+              We'll always let you know about important changes, but you pick
+              what else you want to hear about.
+            </p>
+            <form onSubmit={subTypeFormik.handleSubmit}>
+              <div class="mt-10 space-y-10">
+                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                  <div className="sm:col-span-3">
+                    <label
+                      htmlFor="subscriptionName"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Subscription name
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        {...subTypeFormik.getFieldProps("sub_name")}
+                        required
+                        type="text"
+                        name="sub_name"
+                        id="sub_name"
+                        autoComplete="sub_name"
+                        className="p-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                  <div className="sm:col-span-3">
+                    <label
+                      htmlFor="duration"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Duration
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        {...subTypeFormik.getFieldProps("duration")}
+                        required
+                        type="number"
+                        name="duration"
+                        id="duration"
+                        autoComplete="duration"
+                        className="p-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <button type="submit" className="mt-8 p-2 px-4">
+                ADD
+              </button>
+            </form>
+          </div>
+          <div class="border-b border-gray-900/10 pb-12 p-14">
+            <h2 class="text-base font-semibold leading-7 text-gray-900">
+              Subscription
+            </h2>
+            <p class="mt-1 text-sm leading-6 text-gray-600">
+              We'll always let you know about important changes, but you pick
+              what else you want to hear about.
+            </p>
+
+            <form onSubmit={paperSubFormik.handleSubmit}>
+              <div class="mt-10 space-x-10 flex">
                 <div className="sm:col-span-3">
                   <label
-                    htmlFor="subscriptionName"
+                    htmlFor="sub_id"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    Subscription name
+                    Subscription ID
                   </label>
                   <div className="mt-2">
-                    <input
-                      {...subTypeFormik.getFieldProps("sub_name")}
+                    <select
+                      {...paperSubFormik.getFieldProps("sub_id")}
                       required
-                      type="text"
-                      name="sub_name"
-                      id="sub_name"
-                      autoComplete="sub_name"
-                      className="p-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
-                    />
+                      id="sub_id"
+                      name="sub_id"
+                      autoComplete="sub_id"
+                      className="p-4 block w-32 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset sm:max-w-xs sm:text-sm sm:leading-6"
+                    >
+                      <option value="">Select :</option>
+                      {subType.map((data) => {
+                        return (
+                          <option key={data.sub_name} value={data.sub_id}>
+                            {data.sub_name}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </div>
+                </div>
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="paper_id"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Paper ID
+                  </label>
+                  <div className="mt-2">
+                    <select
+                      {...paperSubFormik.getFieldProps("paper_id")}
+                      required
+                      id="paper_id"
+                      name="paper_id"
+                      autoComplete="paper_id"
+                      className="block w-32 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset sm:max-w-xs sm:text-sm sm:leading-6"
+                    >
+                      <option value="">Select :</option>
+                      {paperID.map((data) => {
+                        return (
+                          <option key={data.name} value={data.paper_id}>
+                            {data.name}
+                          </option>
+                        );
+                      })}
+                    </select>
                   </div>
                 </div>
               </div>
               <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                 <div className="sm:col-span-3">
                   <label
-                    htmlFor="duration"
+                    htmlFor="price"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    Duration
+                    Price
                   </label>
                   <div className="mt-2">
                     <input
-                      {...subTypeFormik.getFieldProps("duration")}
+                      {...paperSubFormik.getFieldProps("price")}
                       required
                       type="number"
-                      name="duration"
-                      id="duration"
-                      autoComplete="duration"
+                      name="price"
+                      id="price"
+                      autoComplete="price"
                       className="p-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
               </div>
-            </div>
-            <button type="submit" className="mt-8 p-2 px-4">
-              ADD
-            </button>
-          </form>
+              <button type="submit" className="mt-8 p-2 px-4">
+                ADD
+              </button>
+            </form>
+          </div>
         </div>
-        <div class="border-b border-gray-900/10 pb-12 p-14">
-          <h2 class="text-base font-semibold leading-7 text-gray-900">
-            Subscription
-          </h2>
-          <p class="mt-1 text-sm leading-6 text-gray-600">
-            We'll always let you know about important changes, but you pick what
-            else you want to hear about.
-          </p>
-
-          <form onSubmit={paperSubFormik.handleSubmit}>
-            <div class="mt-10 space-x-10 flex">
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="sub_id"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Subscription ID
-                </label>
-                <div className="mt-2">
-                  <select
-                    {...paperSubFormik.getFieldProps("sub_id")}
-                    required
-                    id="sub_id"
-                    name="sub_id"
-                    autoComplete="sub_id"
-                    className="p-4 block w-32 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset sm:max-w-xs sm:text-sm sm:leading-6"
-                  >
-                    <option value="">Select :</option>
-                    {subType.map((data) => {
-                      return (
-                        <option key={data.sub_name} value={data.sub_id}>
-                          {data.sub_name}
-                        </option>
-                      );
-                    })}
-                  </select>
-                </div>
-              </div>
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="paper_id"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Paper ID
-                </label>
-                <div className="mt-2">
-                  <select
-                    {...paperSubFormik.getFieldProps("paper_id")}
-                    required
-                    id="paper_id"
-                    name="paper_id"
-                    autoComplete="paper_id"
-                    className="block w-32 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset sm:max-w-xs sm:text-sm sm:leading-6"
-                  >
-                    <option value="">Select :</option>
-                    {paperID.map((data) => {
-                      return (
-                        <option key={data.name} value={data.paper_id}>
-                          {data.name}
-                        </option>
-                      );
-                    })}
-                  </select>
-                </div>
-              </div>
-            </div>
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="price"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Price
-                </label>
-                <div className="mt-2">
-                  <input
-                    {...paperSubFormik.getFieldProps("price")}
-                    required
-                    type="number"
-                    name="price"
-                    id="price"
-                    autoComplete="price"
-                    className="p-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-            </div>
-            <button type="submit" className="mt-8 p-2 px-4">
-              ADD
-            </button>
-          </form>
-        </div>
-      </div>
+      ) : (
+        <h1 className="container m-auto items-center text-center text-2xl">
+          ACCESS DENIED!!!
+        </h1>
+      )}
     </div>
   );
 };
