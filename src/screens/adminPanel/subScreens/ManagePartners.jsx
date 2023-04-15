2420ed001ab80dcd6334bc593de00db1d326342c
SohamSkylar <sohamsarkar2001@gmail.com>
2023-04-15 15:26:32 +0530

    subscription api added

--- src/screens/adminPanel/subScreens/ManagePartners.jsx
+++ src/screens/adminPanel/subScreens/ManagePartners.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Sidebar from "../components/Sidebar";
 import { useFormik } from "formik";
 import { Toaster, toast } from "react-hot-toast";
